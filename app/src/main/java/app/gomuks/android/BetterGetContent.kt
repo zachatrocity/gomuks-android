2019a42dd463afeafa6a4db2152db0f876ad1c1d
Tulir Asokan <tulir@maunium.net>
2025-01-25 21:12:17 +0200

    Add camera options for file picking

--- app/src/main/java/app/gomuks/android/BetterGetContent.kt
+++ app/src/main/java/app/gomuks/android/BetterGetContent.kt
@@ -5,10 +5,16 @@ import android.content.ClipData
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContract
+import androidx.core.content.FileProvider
+import androidx.core.net.toUri
 import app.gomuks.android.BetterGetContent.Result
-import app.gomuks.android.GeckoPrompts.Companion
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 import kotlin.math.min
 
 class BetterGetContent : ActivityResultContract<BetterGetContent.Params, Result?>() {
@@ -56,9 +62,32 @@ class BetterGetContent : ActivityResultContract<BetterGetContent.Params, Result?
         val uri: Uri?,
         val clip: ClipData?,
     )
+    private var currentPhotoTempFile: File? = null
+    private var currentVideoTempFile: File? = null
 
-    override fun createIntent(context: Context, input: Params) =
-        Intent(Intent.ACTION_GET_CONTENT).apply {
+    private fun getPhotoTempURI(context: Context): Uri {
+        val tempFile = currentPhotoTempFile ?: run {
+            val file = File(context.cacheDir, "camera_output.jpg")
+            currentPhotoTempFile = file
+            file.createNewFile()
+            file
+        }
+        return FileProvider.getUriForFile(context, "${context.packageName}.fileprovider", tempFile)
+    }
+
+    private fun getVideoTempURI(context: Context): Uri {
+        val tempFile = currentVideoTempFile ?: run {
+            val file = File(context.cacheDir, "camera_output.mp4")
+            currentVideoTempFile = file
+            file.createNewFile()
+            file
+        }
+        return FileProvider.getUriForFile(context, "${context.packageName}.fileprovider", tempFile)
+    }
+
+    override fun createIntent(context: Context, input: Params): Intent {
+        Log.d(LOGTAG, "Creating intent with type=${input.type}, multiple=${input.multiple}, ${input.mimeTypes?.size} mime types")
+        val getContent = Intent(Intent.ACTION_GET_CONTENT).apply {
             addCategory(Intent.CATEGORY_OPENABLE)
             type = input.type
             putExtra(Intent.EXTRA_LOCAL_ONLY, true)
@@ -66,15 +95,71 @@ class BetterGetContent : ActivityResultContract<BetterGetContent.Params, Result?
                 putExtra(Intent.EXTRA_MIME_TYPES, input.mimeTypes)
             }
             putExtra(Intent.EXTRA_ALLOW_MULTIPLE, input.multiple)
-            Log.d(LOGTAG, "Creating intent with type=${input.type}, multiple=${input.multiple}, ${input.mimeTypes?.size} mime types")
+        }
+        val imageURI = getPhotoTempURI(context)
+        val videoURI = getVideoTempURI(context)
+        val takePicture = Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+            putExtra(MediaStore.EXTRA_OUTPUT, imageURI)
+        }
+        val takeVideo = Intent(MediaStore.ACTION_VIDEO_CAPTURE).apply {
+            putExtra(MediaStore.EXTRA_OUTPUT, videoURI)
+        }
+        val pm = context.packageManager
+        pm.queryIntentActivities(takePicture, 0).forEach {
+            Log.d(LOGTAG, "Found camera activity ${it.activityInfo.packageName}.${it.activityInfo.name}")
+            context.grantUriPermission(it.activityInfo.packageName, imageURI, Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+        }
+        pm.queryIntentActivities(takeVideo, 0).forEach {
+            Log.d(LOGTAG, "Found video activity ${it.activityInfo.packageName}.${it.activityInfo.name}")
+            context.grantUriPermission(it.activityInfo.packageName, videoURI, Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+        }
+        pm.queryIntentActivities(getContent, 0).forEach {
+            Log.d(LOGTAG, "Found content activity ${it.activityInfo.packageName}.${it.activityInfo.name}")
         }
 
-    override fun parseResult(resultCode: Int, intent: Intent?): Result? {
-        return intent.takeIf { resultCode == Activity.RESULT_OK }?.let {
-            Result(
-                uri = it.data,
-                clip = it.clipData,
-            )
+        return Intent.createChooser(getContent, "Select source").apply {
+            putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(getContent, takePicture, takeVideo))
         }
+    }
+
+    override fun parseResult(resultCode: Int, intent: Intent?): Result? {
+        if (resultCode != Activity.RESULT_OK) {
+            Log.d(LOGTAG, "Non-OK result $resultCode for file picking")
+            return null
+        }
+
+        val res: Result
+        val photoFile = currentPhotoTempFile
+        val videoFile = currentVideoTempFile
+        if (intent == null && photoFile != null && videoFile != null) {
+            val currentTS = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
+            currentPhotoTempFile = null
+            currentVideoTempFile = null
+            val destFile: File
+            if (photoFile.length() != 0L) {
+                destFile = File(File(photoFile.parentFile, "upload"), "Camera_$currentTS.jpg")
+                photoFile.renameTo(destFile)
+                videoFile.delete()
+            } else if (videoFile.length() != 0L) {
+                destFile = File(File(videoFile.parentFile, "upload"), "Camera_$currentTS.mp4")
+                videoFile.renameTo(destFile)
+                photoFile.delete()
+            } else {
+                Log.w(LOGTAG, "No result found, temp files are empty")
+                return null
+            }
+            Log.d(LOGTAG, "Using photo/video URI ${destFile.path}")
+            res = Result(uri = destFile.toUri(), clip = null)
+        } else if (intent != null && (intent.data != null || intent.clipData != null)) {
+            Log.d(LOGTAG, "Using result URI ${intent.data} / ${intent.clipData?.itemCount}")
+            res = Result(
+                uri = intent.data,
+                clip = intent.clipData,
+            )
+        } else {
+            Log.w(LOGTAG, "No temp files or result found")
+            return null
+        }
+        return res
     }
 }
